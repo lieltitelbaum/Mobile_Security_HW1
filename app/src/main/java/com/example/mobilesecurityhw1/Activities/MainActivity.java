8ce6f8b2727b708d7c762ae87a4a53f063045d49
Liel Titelbaum <liel.titelbaum1@gmail.com>
2020-11-22 16:50:08 +0200

    Final version. fixes

--- app/src/main/java/com/example/mobilesecurityhw1/Activities/MainActivity.java
+++ app/src/main/java/com/example/mobilesecurityhw1/Activities/MainActivity.java
@@ -2,33 +2,29 @@ package com.example.mobilesecurityhw1.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.res.Configuration;
-import android.location.LocationManager;
-import android.os.BatteryManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.mobilesecurityhw1.Modules.SecureConditions;
+import com.example.mobilesecurityhw1.ConditionsUtils.SecureConditions;
 import com.example.mobilesecurityhw1.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button loginBtn;
+    private EditText passwordTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //find View
-        Button loginBtn = findViewById(R.id.logInBtn);
-        final EditText passwordTxt = findViewById(R.id.passwordTxt);
+        //find Views
+        findViews();
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
                 if (passwordTxt.getText().length() > 0) {
                     if (checkConditions()) {
                         Log.d("loginActivity", "log in successfully");
+
                         startActivity(new Intent(MainActivity.this, LoginSuccessActivity.class));
                         finish();
                     } else {
@@ -49,6 +46,11 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void findViews() {
+        loginBtn = findViewById(R.id.logInBtn);
+        passwordTxt = findViewById(R.id.passwordTxt);
     }
 
     private boolean checkConditions() {
--- app/src/main/java/com/example/mobilesecurityhw1/ConditionsUtils/SecureConditions.java
+++ app/src/main/java/com/example/mobilesecurityhw1/ConditionsUtils/SecureConditions.java
@@ -1,6 +1,5 @@
-package com.example.mobilesecurityhw1.Modules;
+package com.example.mobilesecurityhw1.ConditionsUtils;
 
-import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -9,10 +8,6 @@ import android.location.LocationManager;
 import android.media.AudioManager;
 import android.net.ConnectivityManager;
 import android.os.BatteryManager;
-import android.util.Log;
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 import static android.content.Context.BATTERY_SERVICE;
 
@@ -24,21 +19,25 @@ public class SecureConditions {
     }
 
     public boolean isConnectedToNetwork() {
+        //Return true if device is connected to a network
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
     }
 
     public boolean isRingerModeOn() {
+        //Return true if device is on ringer mode
         AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
         return audioManager.getRingerMode() == AudioManager.RINGER_MODE_NORMAL;
     }
 
     public boolean isGpsEnabled() {
+        //Return true if GPS in activated
         LocationManager manager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE );
         return manager.isProviderEnabled(LocationManager.GPS_PROVIDER);
     }
 
     public boolean isDarkModeOn() {
+        //Return true id dark mode is on
         int nightModeFlags =
                 context.getResources().getConfiguration().uiMode &
                         Configuration.UI_MODE_NIGHT_MASK;
@@ -46,6 +45,7 @@ public class SecureConditions {
     }
 
     public boolean isPhoneInPortraitMode() {
+        //Return true if phone is on portrait node
         return context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
     }
 
